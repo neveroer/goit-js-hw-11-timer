1087d0478cb106dc2b0542d9b23bc2d3b26f9561
neveroer <ivan@ivans-MacBook-Pro.local>
2020-12-02 16:39:18 +0200

    happy NY

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import CountdownTimer from './CountdownTimer';
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Dec 2, 2020, 16:37:00')
+  targetDate: new Date('Dec 31, 2020, 23:59:59')
 });
